8796b4efa6c67406c5879c4189db050e74ee2ff8
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-10-07 12:43:35 +0200

    Display cart item function added

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-contant");
+const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 //CART
 let cart = [];
+//buttons
+let buttonsDOM = [];
 
 //GETTING THE PRODUCTS
 class Products {
@@ -29,9 +31,7 @@ class Products {
     }
   }
 }
-
 //DISPLAY PRODUCTS
-
 class UI {
   displayProducts(products) {
     let result = "";
@@ -55,6 +55,7 @@ class UI {
   //funkcja pobierajaca przycisk z kart, musimy ja dodac po wczystaniu kart
   getBagButtons() {
     const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
     buttons.forEach(button => {
       let id = button.dataset.id;
       let inCard = cart.find(item => item.id === id);
@@ -66,13 +67,44 @@ class UI {
         e.target.innerText = "Dodano do koszyka";
         e.target.disabled = true;
         //get product from products
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
         //add product to the cart
+        cart = [...cart, cartItem];
         //save cart in local storage
+        Storage.saveCart(cart);
         //set cart values
+        this.setCartValues(cart);
         //display cart item
+        this.addCartItem(cartItem);
         //show the cart
       });
     });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.calories * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `<img src=${item.image} alt="product">
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>${item.calories}</h5>
+                        <span class="remove-item" data-id=${item.id}>remove</span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                        <p class="item-amount">${item.amount}</p>
+                        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+                    </div>`;
+    cartContent.appendChild(div);
   }
 }
 
@@ -80,6 +112,13 @@ class UI {
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 document.addEventListener("DOMContentLoaded", () => {
