de21d0ea7c0a3c32c17402026e98ef72d36a91a8
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-07-17 18:37:26 +0200

    First API calls

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import "./main.scss";
+import axios from "axios";
 
 const cartBtn = document.querySelector(".cart__btn");
 const closeCartBtn = document.querySelector(".cart__close");
@@ -330,3 +331,17 @@ document.addEventListener("DOMContentLoaded", () => {
   //     ui.cartLogic();
   //   });
 });
+
+async function getResults(query) {
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  try {
+    const res = await axios(
+      `${proxy}http://forkify-api.herokuapp.com/api/search?&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults("pizza");
