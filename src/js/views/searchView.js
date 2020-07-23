09145ce6d04add00719a267903259ec2dedf8235
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-07-23 16:34:06 +0200

    clearResults function added

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,6 +4,9 @@ export const getInput = () => elements.searchInput.value;
 export const clearInput = () => {
   elements.searchInput.value = "";
 };
+export const clearResults = () => {
+  elements.searchResultList.innerHTML = "";
+};
 
 const renderRecipe = (recipe) => {
   const markup = `
