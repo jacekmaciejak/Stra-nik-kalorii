a95588f74524abd17f5baea5958d05a5cd633670
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-07-23 16:56:15 +0200

    limitRecipeTitle function added

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,15 +8,31 @@ export const clearResults = () => {
   elements.searchResultList.innerHTML = "";
 };
 
+const limitRecipeTitle = (title, limit = 20) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   const markup = `
 <articel class="product">
     <a href="#${recipe.recipe_id}" class="results__link">
     <figure class="results__fig img__container">
-        <img class="product__img" src="${recipe.image_url}" alt="${recipe.title}"/>
+        <img class="product__img" src="${recipe.image_url}" alt="${
+    recipe.title
+  }"/>
     </figure>
     <div class="results">
-        <h4 class="results__name">${recipe.title}</h4>
+        <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
         <p class="results__author">${recipe.publisher}</p>
     </div>
     </a>
